646858271d07ce544a45e3cf0dffbae9d75915b7
taqkla <patricks31121008@gmail.com>
2020-06-10 21:00:45 +0530

    Add files via upload

---
+++ dist/js/about.095663a0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,s){"use strict";s.r(t);var n=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},i=[function(){var e=this,t=e.$createElement,s=e._self._c||t;return s("div",{staticClass:"about"},[s("h1",[e._v("This is the TodoList app v1.0.0. It is developed using Vue.js")])])}],u=s("2877"),a={},l=Object(u["a"])(a,n,i,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=about.095663a0.js.map
